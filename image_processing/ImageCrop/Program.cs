4a85ff8c14c103e6708caac44856837ddad09ba5
Bruno Bernardo <bigsby@live.com>
2024-01-20 13:06:48 +0000

    process multiple files

--- image_processing/ImageCrop/Program.cs
+++ image_processing/ImageCrop/Program.cs
@@ -43,57 +43,78 @@ internal class Program
 
     static (int, int) GetFirst(Image<Rgba32> image, Rgba32 color, int start_x, int start_y)
     {
-        for (var y = 0; y < image.Height; y++)
-            for (var x = 0; x < image.Width; x++)
+        for (var y = start_y; y < image.Height; y++)
+            for (var x = start_x; x < image.Width; x++)
                 if (image[x, y] == color)
                     return (x, y);
         throw new Exception($"{color} not found");
     }
-   
-    
-    static void GetPixel(string file)
+
+
+    static void ExtractCharacters(string file)
     {
+        WriteLine($"Processing: {file}");
         var name = Path.GetFileNameWithoutExtension(file);
         var directory = Path.GetDirectoryName(file) ?? ".";
         using var image = Image.Load<Rgba32>(file);
         var half = image.Width / 2;
         var targetColor = new Rgba32(115, 196, 246, 255);
-        var (start_x, start_y) = GetFirst(image, targetColor, 0, 0);
-        var end_x = start_x;
-        var end_y = start_y;
-        while (end_x < image.Width && image[end_x, start_y] == targetColor)
-            end_x++;
-        while (end_y < image.Height && image[start_x, end_y] == targetColor)
-            end_y++;
-        WriteLine($"{start_x},{start_y} {end_x},{end_y}");
-        var cropped = image.Clone(i => i.Crop(new Rectangle(start_x, start_y, end_x - start_x, end_y - start_y)));
-        cropped.SaveAsPng(Path.Combine(directory, "cropped", $"{name}_first.png"));
-        
+        var (current_x, current_y, end_x, end_y) = (0, 0, 0, 0);
+        var index = 0;
+        while (true)
+        {
+            var (start_x, start_y) = (current_x, current_y);
+            WriteLine($"start {start_x},{start_y} {end_x},{end_y}");
+            var nextFound = false;
+            var newLineAdded = false;
+            for (start_y = current_y; start_y < image.Height && !nextFound; start_y++)
+            {
+                for (start_x = current_x; start_x < image.Width && !nextFound; start_x++)
+                    if (image[start_x, start_y] == targetColor)
+                        nextFound = true;
+                if (!nextFound && !newLineAdded && end_y != 0)
+                {
+                    newLineAdded = true;
+                    start_y = end_y + 10;
+                    current_x = 0;
+                    WriteLine($"new line {start_x},{start_y}");
+                }
+            }
+
+            if (!nextFound || start_x >= image.Width || start_y >= image.Height)
+                break;
+            end_x = start_x;
+            end_y = start_y;
+            while (end_x < image.Width && image[end_x, start_y] == targetColor)
+                end_x++;
+            while (end_y < image.Height && image[start_x, end_y] == targetColor)
+                end_y++;
+            try
+            {
+                var cropped = image.Clone(i => i.Crop(new Rectangle(start_x, start_y, end_x - start_x, end_y - start_y)));
+                var destinationPath = Path.Combine(directory, "cropped", $"{name}_{index++}.png");
+                cropped.SaveAsPng(destinationPath);
+                WriteLine($"{index} {start_x},{start_y} {end_x},{end_y} {end_x - start_x}x{end_y - start_y} {destinationPath}");
+                WriteLine();
+            }
+            catch
+            {
+                WriteLine($"error processing {file} {index}");
+            }
+            (current_x, current_y) = (end_x, start_y);
+        }
+        WriteLine("==================///////////////////========================");
     }
 
     private static void Main(string[] args)
     {
-        if (args.Length != 2)
+        foreach (var filePath in args)
         {
-            WriteLine("filePath count");
-            Environment.Exit(1);
-        }
-        var filePath = args[0];
-        if (!File.Exists(filePath))
-        {
-            WriteLine($"{filePath} not a valid file path");
-            Environment.Exit(1);
-        }
-        GetPixel(filePath) ;
-        return;
-        if (int.TryParse(args[1], out var count))
-        {
-            CropImage(filePath, count);
-        }
-        else
-        {
-            WriteLine("invalid rect values");
-            Environment.Exit(1);
+
+            if (!File.Exists(filePath))
+                WriteLine($"{filePath} not a valid file path");
+            else
+                ExtractCharacters(filePath);
         }
     }
 }
